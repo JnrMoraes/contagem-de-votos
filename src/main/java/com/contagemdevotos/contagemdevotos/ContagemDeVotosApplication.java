c7026666251a51bc77aa2f447989a0e47340a5d5
juniormoraes <contatomoraesjr@gmail.com>
2021-06-19 18:44:54 -0300

    first commit

---
+++ src/main/java/com/contagemdevotos/contagemdevotos/ContagemDeVotosApplication.java
@@ -0,0 +1,13 @@
+package com.contagemdevotos.contagemdevotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContagemDeVotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContagemDeVotosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contagemdevotos/contagemdevotos/ContagemDeVotosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contagemdevotos.contagemdevotos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContagemDeVotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
